c868b967c3b46ed063936abb7a4075c0ae4f101d
AllenSun7 <allensun623@gmail.com>
2022-01-23 00:11:13 -0600

    Amazon OA max power

--- Amazon/MaxPower.py
+++ Amazon/MaxPower.py
@@ -10,3 +10,27 @@ usage = 6 + 3 * 6
 
 
 '''
+from collections import deque
+
+def maxPowerConsumption(bootingPower: list, processingPower: list, powerMax: int) -> int:
+  pq = deque()
+  res = total = i = 0
+  for j in range(len(bootingPower)):
+    total += processingPower[j]
+    # update max value in pq
+    while pq and bootingPower[pq[-1]] < bootingPower[j]:
+      pq.pop()
+    pq.append(j)
+    # left pointer moves while power > powerMax
+    while bootingPower[pq[0]] + (total) * (j - i + 1) > powerMax:
+      total -= processingPower[i]
+      i += 1
+      while pq and pq[0] < i:
+        pq.popleft()
+    
+    res = max(res, j - i + 1)
+    
+  return res
+    
+
+print(maxPowerConsumption([3, 6, 1, 3, 4, 2, 1, 1, 1, 1], [2, 1, 3, 4, 5, 1, 1, 1, 1, 1], 25))
